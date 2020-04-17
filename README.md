6a42e7b03e4f2dab42dec8ee5a159bb0db1bd19a
Oliver Wobst <o.wobst@web.de>
2020-04-17 22:14:34 +0200

    update README, add FileOrganizer

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # useful_scripts
-useful scripts, will add any I come up with
+
+useful scripts, will add any I come up with and write some usage-instructions here
+
+# FileOrganizer
+- cleans up its current working-directory by eighter moving or deleting files by their extensions or names
+- actions for each extension and/or filename configurable within a .JSON-file (config file is automatically created with an example structure if not in directory)
+- do not rename the 'delete' key!, that's a fixed key for files that'll be deleted.
+- JSON-config-file contains a dictionary, keys are directory-names and values the extensions or filenames going into those directorys (directorys are created if not in working-directory)
+- check the example organizer_config.JSON here for the correct format (note that you could omit the dot or put whole filenames there, the script checks if the values are at the end of the filenames so anything goes pretty much)
---
+++ file_organizer/FileOrganizer.py
@@ -0,0 +1,56 @@
+#! python3
+
+import os
+import json
+import sys
+import shutil
+
+# check for json file in working dir, create if not there and stop or continue program if there
+
+DIR = os.getcwd()
+CONFIG_NAME = 'organizer_config.JSON'
+SCRIPT_NAME = os.path.basename(__file__)
+
+print('looking for config-file...')
+if not os.path.exists(os.path.join(DIR, CONFIG_NAME)):
+    print('no config-file found, generating one...')
+    config = {
+        'sample_folder_name_1': ['.pdf', '.doc', '.txt'],
+        'sample_folder_name_2': ['.mp4', '.mov'],
+        'delete': []
+    }
+    with open(CONFIG_NAME, 'w') as outputfile:
+        json.dump(config, outputfile, indent=2)
+    print(f'config file generated as {CONFIG_NAME}. run this again after setting your preferences there.')
+    sys.exit()
+else:
+    print('config-file found. reading config...')
+    with open(CONFIG_NAME) as json_file:
+        config = json.load(json_file)
+
+# loop through all files of working dir, moving/ deleting them according to configs in JSON-file
+
+    for directory, extension in config.items():
+        if not os.path.exists(os.path.join(DIR, directory)) and directory != 'delete':
+            print(f'couldn\'t find directory "{directory}", creating it...')
+            os.mkdir(directory)
+
+        if directory == 'delete':
+            for item in extension:
+                for filename in os.listdir(DIR):
+                    if filename == CONFIG_NAME or filename == SCRIPT_NAME:
+                        continue
+                    elif filename.endswith(item):
+                        os.remove(os.path.join(DIR, filename))
+                        print(f'deleted "{filename}".')
+        else:
+            for item in extension:
+                for filename in os.listdir(DIR):
+                    if filename == CONFIG_NAME or filename == SCRIPT_NAME:
+                        continue
+                    elif filename.endswith(item):
+                        shutil.move(os.path.join(DIR, filename), os.path.join(DIR, directory))
+                        print(f'moved "{filename}" into "{directory}".')
+
+print('finished organizing, you can close this window now. (or press ENTER)')
+input()
